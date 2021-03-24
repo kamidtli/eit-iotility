e2d0bd2e33903f181cdebdabc42f3dfa805444ee
kamidtli <kmidtlid3@gmail.com>
2021-03-24 13:27:18 +0100

    Add types

--- src/utils.tsx
+++ src/utils.tsx
@@ -2,18 +2,22 @@
 
 import DATA_DB from 'data.json';
 import SENSORS_DB from 'sensors.json';
-import { IMeasurement, IParamMeasurement } from 'types';
+import { ISensor, IMeasurement, IParamMeasurement } from 'types';
 
-export function getAllSensors() {
+export function getAllSensors() : ISensor[] {
   return SENSORS_DB.data;
 }
 
-export function getSensorById(id: string) {
-  return SENSORS_DB.data.filter(sensor => sensor.id === id);
+export function getSensorById(id: string) : ISensor[] {
+  return SENSORS_DB.data.filter((s: ISensor) => s.id === id);
+}
+
+export function getSensorsByGroupId(groupId: string) : ISensor[] {
+  return SENSORS_DB.data.filter((s: ISensor) => s.group_id === groupId);
 }
 
 export function getAllSensorMeasurementsById(id: string) : IMeasurement[] {
-  return DATA_DB.data.filter(measurement => measurement.id === id);
+  return DATA_DB.data.filter((m: IMeasurement) => m.id === id);
 }
 
 export function getAllParamMeasurementsById(id: string, param: string) : IParamMeasurement[] {
@@ -29,7 +33,7 @@ export function getAllParamMeasurementsById(id: string, param: string) : IParamM
 }
 
 export function getAllGroupIds() : string[] {
-  const groupIds = SENSORS_DB.data.filter(s => s.group_id !== "0").map(s => s.group_id)
+  const groupIds = SENSORS_DB.data.filter((s: ISensor) => s.group_id !== "0").map(s => s.group_id)
   return removeDuplicates(groupIds);
 }
 
