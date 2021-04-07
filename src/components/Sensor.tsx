10a5fd622a604021046cf9ef69bd45843ae7879c
kmidtlid3@gmail.com <kmidtlid3@gmail.com>
2021-04-07 13:21:39 +0200

    Add params and color coding to group page

--- src/components/Sensor.tsx
+++ src/components/Sensor.tsx
@@ -7,7 +7,7 @@ type SensorProps = {
 function Sensor(props: SensorProps) {
 
   return (
-    <div>{props.sensor.name} ({parseFloat(props.sensor.latitude.toFixed(4))}, {parseFloat(props.sensor.longitude.toFixed(4))})</div>
+    <div>{props.sensor.name}</div>
   )
 
 }
--- src/pages/GroupPage.tsx
+++ src/pages/GroupPage.tsx
@@ -3,12 +3,14 @@ import {
   Link,
   useParams
 } from "react-router-dom";
+import styled from 'styled-components';
 import { Grid } from '@material-ui/core';
 import { ISensor, IGroup, IMeasurement, IMultipleChartData } from 'types';
 import { getAllSensorMeasurementsById, getSensorsByGroupId, getGroupById } from 'utils';
 import Heading from 'components/Heading';
 import Sensor from 'components/Sensor';
 import ParamGraphMultiple from "components/ParamGraphMultiple";
+import { chartColors } from "static";
 
 interface SensorPageProps {
   id: string
@@ -17,27 +19,32 @@ interface SensorPageProps {
 function GroupPage() {
 
   const { id } = useParams<SensorPageProps>();
-  const [data, setData] = useState<IMultipleChartData[] | []>();
   const [sensors, setSensors] = useState<ISensor[] | []>();
   const [valueCounter, setValueCounter] = useState<number>(0);
   const [group, setGroup] = useState<IGroup>();
 
   useEffect(() => {
-    let groupData:{ [timestamp: string]: { [value: string]: {}} } = {}
     const sensors: ISensor[] = getSensorsByGroupId(id);
-    sensors.forEach((s: ISensor, index: number) => {
+    setSensors(sensors);
+    setValueCounter(sensors.length);
+    setGroup(getGroupById(id));
+  }, [id])
+
+  const getParam = (param: string) : IMultipleChartData[] => {
+    let groupData:{ [timestamp: string]: { [value: string]: {}} } = {}
+    sensors?.forEach((s: ISensor, index: number) => {
       const measurements: IMeasurement[] = getAllSensorMeasurementsById(s.id);
       measurements.forEach((m: IMeasurement) => {
         let currentTimestamp = m.timestamp;
         let valueString = "value" + (index + 1);
         if (groupData.hasOwnProperty(currentTimestamp)) {
-          groupData[currentTimestamp][valueString] = m.pH;
+          groupData[currentTimestamp][valueString] = m[param as keyof IMeasurement];
         } else {
-          groupData[currentTimestamp] = { [valueString]: m.pH };
+          groupData[currentTimestamp] = { [valueString]: m[param as keyof IMeasurement] };
         }
       });
     });
-    const finalData = []
+    const finalData = [];
     for (let timestamp in groupData) {
       if (groupData.hasOwnProperty(timestamp)) {
         let tempObj = { date: new Date(timestamp) };
@@ -45,32 +52,40 @@ function GroupPage() {
         finalData.push(fullTempObj);
       }
     }
-    setData(finalData);
-    setSensors(sensors);
-    setValueCounter(sensors.length);
-    setGroup(getGroupById(id));
-  }, [id])
+    return finalData;
+  }
 
   return (
     <div>
       <Link to="/">Tilbake til sensorkart</Link>
-      {data ?
+      {sensors ?
         (
         <div>
           <Heading title={group && group.name} />
           <Grid container spacing={3}>
-            <Grid item xs={12} lg={6}>
-              <ParamGraphMultiple title="pH" data={data} numOfValues={valueCounter} id={1}/>
-            </Grid>
             <Grid item xs={12}>
-              <Heading subtitle="Sensorer i denne gruppen" />
-              {sensors && sensors.map((s: ISensor) => {
+              {sensors && sensors.map((s: ISensor, index: number) => {
                 return (
-                  <Link to={`/sensors/${s.id}`} key={s.id}>
-                    <Sensor sensor={s} />
-                  </Link>
+                  <DotLink>
+                    <ColorDot color={chartColors[index]} />
+                    <Link to={`/sensors/${s.id}`} key={s.id}>
+                      <Sensor sensor={s} />
+                    </Link>
+                  </DotLink>
                 )
               })}
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <ParamGraphMultiple title="pH" data={getParam("pH")} numOfValues={valueCounter} id={1}/>
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <ParamGraphMultiple title="Turbiditet" data={getParam("turbidity")} numOfValues={valueCounter} id={2}/>
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <ParamGraphMultiple title="Temperatur" data={getParam("temperature")} numOfValues={valueCounter} id={3}/>
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <ParamGraphMultiple title="Konduktivitet" data={getParam("conductivity")} numOfValues={valueCounter} id={4}/>
             </Grid>
           </Grid>
         </div>
@@ -80,5 +95,19 @@ function GroupPage() {
     </div>
   );
 }
+
+const DotLink = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const ColorDot = styled.div`
+  margin-right: 10px;
+  width: 16px;
+  height: 16px;
+  border-radius: 8px;
+  background-color:${props => props.color};
+`;
 
 export default GroupPage;
