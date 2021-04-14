865e6e3a91effa63ee123efa8dd625693ddf1a53
kamidtli <kmidtlid3@gmail.com>
2021-04-14 12:37:35 +0200

    Update heading style

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 interface HeadingProps {
   title?: string,
   subtitle?: string
@@ -6,10 +8,19 @@ interface HeadingProps {
 function Heading(props: HeadingProps) {
   return (
     <div>
-      <h2>{props.title}</h2>
-      <h5>{props.subtitle}</h5>
+      <Title>{props.title}</Title>
+      <Subtitle>{props.subtitle}</Subtitle>
     </div>
   )
 }
+
+const Title = styled.h2`
+  margin-bottom: 5px;
+`;
+
+const Subtitle = styled.h5`
+  margin-top: 5px;
+  font-weight: normal;
+`;
 
 export default Heading;
